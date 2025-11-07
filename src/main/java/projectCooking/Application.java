c8fefdf3016008d4fce51609223988fe2c3931de
tailoac37 <tailoac37@users.noreply.github.com>
2025-11-07 18:21:27 +0700

    fix url 3

--- src/main/java/projectCooking/Application.java
+++ src/main/java/projectCooking/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main (String args[])
 	{
+		System.out.println("DB_URL=" + System.getenv("DB_URL"));
+		System.out.println("DB_USER=" + System.getenv("DB_USER"));
+		System.out.println("DB_PASSWORD=" + System.getenv("DB_PASSWORD"));
+
 		SpringApplication.run(Application.class, args) ; 
 	}
 }	
